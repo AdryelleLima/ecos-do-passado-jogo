dd55c08d73122e0b0bd55718ff4d9f87cc974bce
H_Adryelle_Lima <161168768+AdryelleLima@users.noreply.github.com>
2026-08-04 20:41:31 -0300

    Implement Sala class with attributes and methods
    Added attributes and methods to manage room connections and items.

--- src/Sala.java
+++ src/Sala.java
@@ -1,3 +1,39 @@
-public class Sala {
-    
+import java.util.HashMap;
+import java.util.Map;
+
+public class Sala{
+    private String nome;
+    private String descricao;
+    private Map<String, Sala> saidas; // Conecta a direção ("norte","sul", etc.) com outra Sala
+    private String itemPresente; // Nome do item/documento que está na sala
+
+    //Construtor
+    public Sala(String nome, String descricao){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.saidas = new HashMap<>();
+        this.itemPresente = null;
+    }
+
+
+    //Define qual sala fica em qual direção
+    public void definirSala(String direcao, Sala salaVizinha){
+        saidas.put(direção.toLowerCase(), salaVizinha);
+    }
+    public Sala pegarSaida(String direcao){
+        return saidas.get(direcao.toLowerCase());
+    }
+    //Getters e Setters para nome, descrição e itens
+    public String pegarNome(){
+        return nome;
+    }
+    public String pegarDescricao(){
+        return descricao;
+    }
+    public String pegarItemPresente(){
+        return itemPresente;
+    }
+    public String conjuntoItemPresente(String itemPresente){
+        this.itemPresente = itemPresente;
+    }
 }
