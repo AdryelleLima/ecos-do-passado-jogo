07411be71dfe0e7e7a755b8e50dcae55c1519d4e
H_Adryelle_Lima <161168768+AdryelleLima@users.noreply.github.com>
2026-08-04 20:23:11 -0300

    Implement main menu for game application

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main{
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        boolean noMenu = true;
+
+        while(noMenu){
+            exibirMenuPrincipal();
+            System.out.print("Escolha uma opção > ");
+            String opcao = scanner.nextLine().trim();
+
+            switch (opcao){
+                case "1":
+                    continuarJogo();
+                    break;
+                case "2":
+                    iniciarNovoJogo();
+                    break;
+                case "3":
+                    configuracaoJogo();
+                    break;
+                case "4":
+                    sobreJogo();
+                    break;
+                case "5":
+                    System.out.println("\nSaindo do jogo... Até a próxima!");
+                    noMenu = false;
+                    break;
+                default:
+                    System.out.println("\nOpção inválida! Digite 1, 2, 3, 4 ou 5.");
+            }
+        }
+        scanner.close();
+    }
+    private static void exibirMenuPrincipal(){
+        System.out.println("\n==================================================");
+        System.out.println("    ECOS DO PASSADO: O SEGREDO DA CASA ABANDONADA   ");
+        System.out.println("==================================================");
+        System.out.println("1. Continuar Jogo");
+        System.out.println("2. Iniciar Novo Jogo");
+        System.out.println("2. Configuração");
+        System.out.println("4. Sobre");
+        System.out.println("5. Sair");
+        System.out.println("==================================================");
+    }
+    }
+}
