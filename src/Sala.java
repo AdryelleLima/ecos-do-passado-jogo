545ccbed7e656babb39a140fa77996d227e915ec
H_Adryelle_Lima <161168768+AdryelleLima@users.noreply.github.com>
2026-08-04 21:25:49 -0300

    Refactor itemPresente to use Item class
    Updated itemPresente to use Item class instead of String.

--- src/Sala.java
+++ src/Sala.java
@@ -1,11 +1,12 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class Sala{
     private String nome;
     private String descricao;
     private Map<String, Sala> saidas; // Conecta a direção ("norte","sul", etc.) com outra Sala
-    private String itemPresente; // Nome do item/documento que está na sala
+    private Item itemPresente; // Nome do item/documento que está na sala ->  Agora usa a classe Item em vez de String!
 
     //Construtor
     public Sala(String nome, String descricao){
@@ -15,7 +16,6 @@ public class Sala{
         this.itemPresente = null;
     }
 
-
     //Define qual sala fica em qual direção
     public void definirSala(String direcao, Sala salaVizinha){
         saidas.put(direção.toLowerCase(), salaVizinha);
@@ -23,17 +23,17 @@ public class Sala{
     public Sala pegarSaida(String direcao){
         return saidas.get(direcao.toLowerCase());
     }
-    //Getters e Setters para nome, descrição e itens
+    //Getters e Setters = pegar e conjunto para nome, descrição e itens
     public String pegarNome(){
         return nome;
     }
     public String pegarDescricao(){
         return descricao;
     }
-    public String pegarItemPresente(){
+    public Item pegarItemPresente(){
         return itemPresente;
     }
-    public String conjuntoItemPresente(String itemPresente){
+    public void conjuntoItemPresente(Item itemPresente){
         this.itemPresente = itemPresente;
     }
 }
