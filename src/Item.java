befa8e17933f4c78f24eeed91fd84ec3eee2b8e2
H_Adryelle_Lima <161168768+AdryelleLima@users.noreply.github.com>
2026-08-04 21:21:01 -0300

    Implement Item class with properties and methods
    Added properties and methods to the Item class.

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,22 @@
-public class Item {
-    
+public class Item{
+    private String nome;
+    private String descricao;
+    private boolean coletavel; // true = pode pegar, false = objeto fixo do cenário
+
+    // Construtor
+    public Item(String nome, String decricao, boolean coletavel){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.coletavel = coletavel;
+    }
+    // Getters e Setters = pegar e conjunto
+    public String pegarNome(){
+        return nome;
+    }
+    public String pegarDescricao(){
+        return descricao;
+    }
+    public boolean eColetavel(){
+        return coletavel;
+    }
 }
