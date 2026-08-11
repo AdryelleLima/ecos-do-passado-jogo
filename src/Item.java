5d508a3f89a5df07c743d4183e6ef28d69e54995
H_Adryelle_Lima <161168768+AdryelleLima@users.noreply.github.com>
2026-08-11 19:23:58 -0300

    Implement hidden item management in Item class
    Added methods to manage hidden items within an Item.

--- src/Item.java
+++ src/Item.java
@@ -2,13 +2,25 @@ public class Item{
     private String nome;
     private String descricao;
     private boolean coletavel; // true = pode pegar, false = objeto fixo do cenário
-
+    private Item itemEscondido; // Guarda o item escondido dentro deste item, se houver.
     // Construtor
     public Item(String nome, String descricao, boolean coletavel){
         this.nome = nome;
         this.descricao = descricao;
         this.coletavel = coletavel;
+        this.itemEscondido = null;
     }
+    // Metodos para gerenciar itens escondidos
+    public Item pegarItemEscondido(){
+        return itemEscondido;
+    }
+    public void esconderItem(Item item){
+        this.itemEscondido = item;
+    }
+    public boolean temItemEscondido(){
+        return itemEscondido != null;
+    }
+
     // Getters e Setters = pegar e conjunto
     public String pegarNome(){
         return nome;
