82f5a11f81f18e030f013b962806829c3e7fa230
H_Adryelle_Lima <161168768+AdryelleLima@users.noreply.github.com>
2026-08-14 23:27:47 -0300

    Enhance Item class with locking and search description
    Added functionality for locked items and post-search descriptions.

--- src/Item.java
+++ src/Item.java
@@ -1,23 +1,56 @@
 public class Item{
     private String nome;
     private String descricao;
+    private String descricaoAposBusca;
     private boolean coletavel; // true = pode pegar, false = objeto fixo do cenário
     private Item itemEscondido; // Guarda o item escondido dentro deste item, se houver.
     private String conteudoTexto;
+
+
+    private boolean trancado = false;
+    private String chaveNecessaria = "";
 
     // Construtor
     public Item(String nome, String descricao, boolean coletavel){
         this.nome = nome;
         this.descricao = descricao;
+        this.descricaoAposBusca = null;
         this.coletavel = coletavel;
         this.itemEscondido = null;
         this.conteudoTexto = null;
     }
-    // --- MÉTODOS PARA CONTEÚDO DE LEITURA ---
-    public String pegarConteudoTexto() {
-        return conteudoTexto;
+    public boolean estaTrancado() {
+        return trancado;
     }
 
+    public void trancar(String nomeChave) {
+        this.trancado = true;
+        this.chaveNecessaria = nomeChave;
+    }
+
+    public boolean destrancarCom(String nomeChave) {
+        if (trancado && chaveNecessaria.equalsIgnoreCase(nomeChave)) {
+            this.trancado = false;
+            return true;
+        }
+        return false;
+    }
+    public String pegarChaveNecessaria() {
+        return chaveNecessaria;
+    }
+    public void ConjuntoDescricaoApoBusca(String novaDescricao){
+        this.descricaoAposBusca = novaDescricao;
+    }
+    public String pegarDescricaoAposBusca(){
+        return descricaoAposBusca;
+    }
+    // --- MÉTODOS PARA CONTEÚDO DE LEITURA ---
+    public String pegarConteudoTexto() {
+        if(!temItemEscondido() && descricaoAposBusca != null){
+            return descricaoAposBusca;
+        }
+        return descricao;
+    }
     public void conjuntoConteudoTexto(String conteudoTexto) {
         this.conteudoTexto = conteudoTexto;
     }
